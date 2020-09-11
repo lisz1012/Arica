fbe484d525d10561311acd8552672df0ee8f13e3
lisz1012 <lisz1012@163.com>
2020-09-10 23:35:02 -0700

    Initial commit

---
+++ src/main/java/com/lisz/arica/AricaApplication.java
@@ -0,0 +1,13 @@
+package com.lisz.arica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AricaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisz/arica/AricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisz.arica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
